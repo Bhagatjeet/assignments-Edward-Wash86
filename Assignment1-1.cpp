a44b246c23f355799e9be8247eaf3625349547e0
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-09-06 23:43:06 +0000

    I am able to import the data from the student file.

--- Assignment1-1.cpp
+++ Assignment1-1.cpp
@@ -1,30 +1,60 @@
 /*
 Create a structure type "student".
-Read rge text file "students.txt" and then fill up the struct "Studnets".
+Read the text file "students.txt" and then fill up the struct "Studnets".
 Make a function "findstudnet" to find the particular record with the "id".
 if we give the is ""10009, show all the information about the studnet "Mike" including the total scores and average.
 */
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-struct students{
+struct students {
  int id;
  string name;
  int score1, score2;
- double scoreavg;
+ float scoreavg;
 };
 
 void findstudnets(int);
 
 int main() {
-  fstream studentfile;
+  fstream newfile;
 
-  studentfile.open("students.txt", ios :: in);
+  students firstclass[10];
+
+  newfile.open("students.txt", ios:: in);
+
+  if(newfile){
+    cout << "File has been opened" << endl;
+  }
+
+  while(!newfile.eof()){
+    for(int i = 0; i < sizeof(newfile); i++){
+    newfile >> firstclass[i].id;
+
+    newfile >> firstclass[i].name;
+
+    newfile >> firstclass[i].score1;
+
+    newfile >> firstclass[i].score2;
+    
+    firstclass[i].scoreavg = (static_cast<double>(firstclass[i].score2) + static_cast<double>(firstclass[i].score1)) / 2;
+
+    }
+  }
+
+  newfile.close();
+
+  for(int i = 0; i < 0; i++){
+    
+  }
+
+
 }
 
-void findstudnets(int id){
+// void findstudnets(int id){
 
-}
+// }
