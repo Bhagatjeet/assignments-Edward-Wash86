4572640b483f780d2b29b8161297d37f6c6d6f85
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-09-07 12:49:48 +0000

    Finished

--- Assignment1-1.cpp
+++ Assignment1-1.cpp
@@ -2,7 +2,7 @@
 Create a structure type "student".
 Read the text file "students.txt" and then fill up the struct "Studnets".
 Make a function "findstudnet" to find the particular record with the "id".
-if we give the is 10009, show all the information about the studnet "Mike" including the total scores and average.
+if we give the is ""10009, show all the information about the studnet "Mike" including the total scores and average.
 */
 
 #include <iostream>
@@ -15,8 +15,8 @@ using namespace std;
 struct students {
  int id;
  string name;
- int score1 = 0, score2 = 0;
- float scoreavg;
+ double score1,score2;
+ double scoreavg;
 };
 
 void findstudnets(int);
@@ -27,67 +27,48 @@ int main() {
 
   students firstclass[10];
   int input;
+  
+  newfile.open("students.txt", ios:: in);
 
-   
-
-  do{
-    cout << "Menu:" << endl;
-    cout << "1. Search" << endl;
-    cout << "2. print file" << endl;
-    cin >> input;
-
-    switch(input){
-    case 1: newfile.open("students.txt", ios:: in);
-      if(newfile){
-      cout << "File has been opened" << endl;
-      }
-
-      while(!newfile.eof()){
-        for(int i = 0; i < sizeof(newfile); i++){
-        newfile >> firstclass[i].id;
-
-        newfile >> firstclass[i].name;
-
-        newfile >> firstclass[i].score1;
-
-        newfile >> firstclass[i].score2;
-        }
-        newfile.close();
-      }
-      // for(int i = 0; i < sizeof(newfile); i++){
-      //   firstclass[i].scoreavg = (static_cast<double>(firstclass[i].score1) + static_cast<double>(firstclass[i].score2)) / 2;
-      // }
-
-      cout << "Please enter a student ID: " << endl;
-      cin >> input;
-      for(int i = 0; i < sizeof(newfile); i++){
-      if(input == firstclass[i].id){
-        cout << "Student information found" << endl;
-        cout << "Student Number: " << "Student Name: " << "First Score: " << "Second Score: " << "Score Average: " << endl;
-        cout << firstclass[i].id << setw(11)<< " " << firstclass[i].name << setw(11)<<  " " << firstclass[i].score1 << setw(13)<< " " << firstclass[i].score2 << setw(13)<< " " <<  firstclass[i].scoreavg;
-      }
-    }
-    
-    break;
-    case 2: for(int i = 0; i < sizeof(newfile); i++ ){
-
-    }
+  if(newfile){
+    cout << "File has been opened" << endl;
   }
 
-  } while(input != 0);
+  while(!newfile.eof()){
+    for(int i = 0; i < sizeof(newfile); i++){
+      newfile >> firstclass[i].id;
 
-  
+      newfile >> firstclass[i].name;
+
+      newfile >> firstclass[i].score1;
+
+      newfile >> firstclass[i].score2;
+      
+      //firstclass[i].scoreavg = (static_cast<double>(firstclass[i].score2) + static_cast<double>(firstclass[i].score1)) / 2;
+    } 
+    newfile.close();
+  } 
 
 
+  for(int i = 0; i < 10; i++){
+    firstclass[i].scoreavg = (firstclass[i].score1 + firstclass[i].score2) / 2;
+    
+  }
+
+  while(input != 0){
+    cout << "Please enter a student ID or enter ZERO to quit." << endl;
+    cin >> input;
+    for(int i = 0; i < 10; i++){
+      if(input == firstclass[i].id){
+        cout << "Student information found" << endl;
+        cout << endl;
+        cout << "Student Number: " << "Student Name: " << "First Score: " << "Second Score: " << "Score Average: " << endl;
+        cout << firstclass[i].id << setw(11)<< " " << firstclass[i].name << setw(10)<<  " " << firstclass[i].score1 << setw(10)<< " " << firstclass[i].score2 << setw(12)<< " " <<  firstclass[i].scoreavg;
+        cout << endl;
+        cout << endl;
+      }
+    }
+  }
 }
 
-// void printfile(students s[]){
-//   for(int i = 0; i < 10; i++){
-//     cout << s[i].id << " " << s[i].name << " " << s[i].score1  << " "<< s[i].score2 << " " << s[i].scoreavg;
-//     cout << endl;
-//   }
-// }
-
-// void findstudnets(int id){
-
-// }
+  
