69c738c5a5bbf8dd7b5cfcc4c06fae50714c2a37
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-09-08 02:08:58 +0000

    function not working

--- Assignment1-1.cpp
+++ Assignment1-1.cpp
@@ -13,14 +13,14 @@ if we give the is ""10009, show all the information about the studnet "Mike" inc
 using namespace std;
 
 struct students {
- int id;
+ int id, sum;
  string name;
  double score1,score2;
  double scoreavg;
 };
 
-void findstudnets(int);
-void printfile(students &);
+void findstudents(students);
+
 
 int main() {
   fstream newfile;
@@ -44,31 +44,52 @@ int main() {
 
       newfile >> firstclass[i].score2;
       
-      //firstclass[i].scoreavg = (static_cast<double>(firstclass[i].score2) + static_cast<double>(firstclass[i].score1)) / 2;
     } 
     newfile.close();
   } 
 
 
   for(int i = 0; i < 10; i++){
-    firstclass[i].scoreavg = (firstclass[i].score1 + firstclass[i].score2) / 2;
+    firstclass[i].sum = firstclass[i].score1 + firstclass[i].score2;
+    firstclass[i].scoreavg = (firstclass[i].sum) / 2;
     
   }
+  for(int i = 0; i < 10; i++)
+    findstudents(firstclass[i]);
 
+  newfile.open("students.txt", ios:: out);
+  while(!newfile.eof()){
+    for(int i = 0; i < sizeof(newfile); i++){
+      newfile << firstclass[i].id;
+
+      newfile << firstclass[i].name;
+
+      newfile << firstclass[i].score1;
+
+      newfile << firstclass[i].score2;
+      
+      newfile << firstclass[i].sum;
+
+      newfile << firstclass[i].scoreavg;
+      
+    } 
+  }
+}
+
+void findstudents(students classes[]){
+  int input;
   while(input != 0){
     cout << "Please enter a student ID or enter ZERO to quit." << endl;
     cin >> input;
     for(int i = 0; i < 10; i++){
-      if(input == firstclass[i].id){
+      if(input == classes[i].id){
         cout << "Student information found" << endl;
         cout << endl;
-        cout << "Student Number: " << "Student Name: " << "First Score: " << "Second Score: " << "Score Average: " << endl;
-        cout << firstclass[i].id << setw(11)<< " " << firstclass[i].name << setw(10)<<  " " << firstclass[i].score1 << setw(10)<< " " << firstclass[i].score2 << setw(12)<< " " <<  firstclass[i].scoreavg;
+        cout << "Student Number: " << "Student Name: " << "First Score: " << "Second Score: " << "Sum of score" << "Score Average: " << endl;
+        cout << classes[i].id << setw(11)<< " " << classes[i].name << setw(10)<<  " " << classes[i].score1 << setw(10)<< " " << classes[i].score2 << setw(12)<< " " << classes[i].sum << " "<< classes[i].scoreavg;
         cout << endl;
         cout << endl;
       }
     }
   }
 }
-
-  
