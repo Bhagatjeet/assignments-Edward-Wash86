cb82e242b2890fded528d4c2e66de329dd89eeba
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-09-23 01:32:30 +0000

    Getting the general idea of the program started.

---
+++ Assignment8-5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector> 
+
+
+
+int main(){
+
+  vector <int> numbers(10);
+
+  const int N = 10;
+  vector <int> IDs(N);
+  vector <string> names(N);
+
+  /*
+  for(int i = 0: i < N; i++){
+    minval = numbers[i];
+    minid = i;
+    for(int j = i+1; i < N; j++){
+      if(minval > numbers[j]){
+        minval= numbers[j];
+        minid = j;
+      }
+    }
+    numbers[minid] = numbers[i];
+    numbers[i] = minval;
+  }
+  */
+
+  // sort() - sort(numbers.begin(), numbers.end())
+}
