ae2d488b62d6c84f49739dddbcf9deff70958dcb
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-09-27 10:36:54 +0000

    Completed and polished.

--- Assignment9-1.cpp
+++ Assignment9-1.cpp
@@ -9,11 +9,20 @@ int main(){
   double number[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.51, 13.8, 14.7, 19.8, 20.0};
   double *ptr;
 
-  cout << setprecision(4);
+  cout << "Memory addresses: " << endl;
 
   for(int i = 0; i < N; i++){
     ptr = &number[i];
-    cout << *ptr << " ";
+    cout << "Element (" << i << ")is: " << ptr << endl; 
+  }
+
+  cout << endl;
+
+  cout << "Element contents: " << endl;
+
+  for(int i = 0; i < N; i++){
+    ptr = &number[i];
+    cout << "The element (" << i << ") of the array number contains ; " << *ptr  << endl;
   }
 
   // cout << "Data stored in the memory address: ";
