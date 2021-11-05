41b5e80ab8e176a7018884885675c3ca463f6bc2
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-05 04:21:48 +0000

    update

--- Assignment13-4/stack.hpp
+++ Assignment13-4/stack.hpp
@@ -1,23 +1,28 @@
-// class declaration
-#ifndef STACK_H
-#define STACK_H
+#include <iostream>
+#include "Stack.hpp"
 #include <vector>
+#include <stack>
 using namespace std;
 
-class stack1{
-  private:
-    vector <int> pool;
-  public:
-    stack1(); //creates the vector <int> with the default of 10 elemts; // revurse()
-    stack1(int n); //creates the vector < int > with n elements; reverse()
-    void clear(); // clear the stack. Use the vector member clear();
-    void isEmpty(); //check to see if the stack is empty.
-    void push(int el); //put the element el on th etop of the stack //push_back()
-    void pop(); //Take the topmost elemnt from the stack.
-    int topEl();
-    int getSize();
-    void printAll();
+/*
+The Private data of stack
+ - vector < int > pool
+ The operations are aas follows:
+  - stack(): creates the vector <int> with the default of 10 elemts; // revurse()
+  - stack(n): creates the vector < int > with n elements; reverse()
+  - clear() - clear the stack. Use the vector member clear();
+  - isEmpty(): check to see if the stack is empty.
+  - push(el) - put the element el on th etop of the stack //push_back()
+  - pop() - Take the topmost elemnt from the stack.
+  - topEl() - return the topmost elemnt in the stack without removing it. // back()- getSize();
+  - printAll;
+  
+  Important: swap() relocation
 
-};
+*/
 
-#endif
+int main(){
+  stack < int > myStack[20];
+  
+  
+}
