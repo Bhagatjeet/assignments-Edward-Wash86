1d6db46c42c751ed99d90167dca56edddd7dc3dd
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-05 02:03:08 +0000

    Started

---
+++ Assignment13-4/13-4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+
+/*
+The Private data of stack
+ - vector < int > pool
+ The operations are aas follows:
+  - stack(): creates the3 vector <int> with the default of 10 elemts; // revurse()
+  - stack(n): creates the vector < int > with n elements; reverse()
+  - clear() - clear the stack. Use the vector member clear();
+  - isEmpty(): check to see if the stack is empty.
+  - push(el) - put the element el on th etop of the stack //push_back()
+  - pop() - Take the topmost elemnt in the stack without removing it.
+  - topEl() - return the topmost elemnt in the stack without removing it. // back()- getSize();
+  - printAll;
+  
+   
+
+*/
+int main(){
+  stack <int> myStack;
+
+  myStack[20];
+  cout << "Size is: " << myStack.size() << endl;
+}
