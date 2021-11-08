92d7094081b68d06e3f9756807f659a2245d2724
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-08 12:19:46 +0000

    updated

--- Chapter14-2/Assignment14-2.cpp
+++ Chapter14-2/Assignment14-2.cpp
@@ -1,148 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <string> 
+#include "Assignment14-2.hpp" 
+
 
 // Test build
 
 using namespace std;
 
 
-class A{
-  private:
-    int p_A = 100;
-    friend class B;// freind relation does not go both ways. (friend class)
-};
-class B{
-  private:
-    int p_B = 50;
-  public:
-    void show(A& x){
-      cout << x.p_A;//accessing private member.(friend class)
-    }
-    friend class A;
-    friend void printAll(A& x, B& y){
-      cout << x.p_A << endl;
-      cout << y.p_B << endl;
-    }
-};
-class Student1{
-  private:
-    int sid;
-    string sname;
-    vector <double> score;
-  public:
-  Student1(){
-    sid = 0;
-    sname = "No_Name";
-    // srand(time(NULL));
-    // for(int i = 0; i < 5; i++){
-    //   score.push_back((rand() % 50));
-    //   cout << score[i] << " " << endl;
-    };
-  
-  Student1(string n, int id, vector <double> &sc){
-    sname = n;
-    sid = id;
-    for(int i = 0; i < sc.size(); i++){
-      score.push_back(sc[i]);
-    }
-  };
-  void setID(int id){
-    sid = id;
-  }
-  void setName(string name){
-    sname = name;
-  }
-  void setScores(vector <double> &sc){
-    for(int i = 0; i < sc.size(); i++){
-      score.push_back(sc[i]);
-    }
-  }
-  void getScore(){
-    cout << "Scores: ";
-    for(int i = 0; i < score.size(); i++){
-      cout << score[i] << endl;
-    }
-  }
-  string getName(){
-    return sname;
-  }
-  int getID(){
-    return sid;
-  }
 
-  friend class Student2;
-  
-
-};
-class Student2{
-  private:
-    int sid;
-    string sname;
-    vector <double> score;
-  public:
-  Student2(){
-    sid = 0;
-    sname = "No_Name";
-    // srand(time(NULL));
-    // for(int i = 0; i < 5; i++){
-    //   score.push_back((rand() % 50));
-    //   cout << score[i] << " " << endl;
-    };
-  
-  Student2(string n, int id, vector <double> &sc){
-    sname = n;
-    sid = id;
-    for(int i = 0; i < sc.size(); i++){
-      score.push_back(sc[i]);
-    }
-  };
-  void setID(int id){
-    sid = id;
-  }
-  void setName(string name){
-    sname = name;
-  }
-  void setScores(vector <double> &sc){
-    for(int i = 0; i < sc.size(); i++){
-      score.push_back(sc[i]);
-    }
-  }
-  void getScore(){
-    cout << "Scores: ";
-    for(int i = 0; i < score.size(); i++){
-      cout << score[i] << endl;
-    }
-  }
-  string getName(){
-    return sname;
-  }
-  int getID(){
-    return sid;
-  }
-  friend class Student1;
-  friend int largerTotalScore(Student1& x ,Student2& y){
-    double xTotal = 0;
-    double yTotal = 0;
-    int master;
-    for(int i = 0; i < 5; i++){
-
-      xTotal += x.score[i];
-      yTotal += y.score[i];
-    }
-    if(xTotal < yTotal){
-      cout << "from the second peramiter: ";
-      master = yTotal;
-      }
-    else if(yTotal < xTotal){
-      cout << "from the first peramiter: ";
-      master = xTotal;
-    }
-    return master;
-  }
-
-};
 
 
 
@@ -158,15 +25,13 @@ int main() {
   Student1 person1("bob",100093,set1);
   Student2 person2("Samuel", 10000565, set2);
 
-  
-  A a;
-  B b;
-  
-  b.show(a);
-
-  printAll(a,b);
   cout << endl;
-  cout << "The largets score is " << largerTotalScore(person1 , person2) << endl;;
+  printStudent(person1);
+  cout << endl;
+  printStudent(person1);
+  cout << endl;
+  cout << "The largets score is " << largerTotalScore(person1 , person2) << endl;
+  cout << endl;
   person1.getScore();
   cout << endl;
   person2.getScore();
---
+++ Chapter14-2/Assignment14-2.hpp
@@ -0,0 +1,160 @@
+#ifndef ASSIGNMENT14_H
+#define ASSIGNMENT14_H
+
+#include <iostream>
+#include <vector> 
+#include <ctime>
+
+using namespace std;
+
+class Student1{
+  private:
+    int sid;
+    string sname;
+    vector <double> score;
+  public:
+  Student1(){
+    sid = 0;
+    sname = "No_Name";
+    // srand(time(NULL));
+    // for(int i = 0; i < 5; i++){
+    //   score.push_back((rand() % 50));
+    //   cout << score[i] << " " << endl;
+    };
+  
+  Student1(string n, int id, vector <double> &sc){
+    sname = n;
+    sid = id;
+    for(int i = 0; i < sc.size(); i++){
+      score.push_back(sc[i]);
+    }
+  };
+  void setID(int id){
+    sid = id;
+  }
+  void setName(string name){
+    sname = name;
+  }
+  void setScores(vector <double> &sc){
+    for(int i = 0; i < sc.size(); i++){
+      score.push_back(sc[i]);
+    }
+  }
+  void getScore(){
+    cout << "Scores: ";
+    for(int i = 0; i < score.size(); i++){
+      cout << score[i] << endl;
+    }
+  }
+  string getName(){
+    return sname;
+  }
+  int getID(){
+    return sid;
+  }
+  friend class Student2;
+  // friend int largerTotalScore(Student1& x ,Student2& y){
+  //   double xTotal = 0;
+  //   double yTotal = 0;
+  //   int master;
+  //   for(int i = 0; i < 5; i++){
+
+  //     xTotal += x.score[i];
+  //     yTotal += y.score[i];
+  //   }
+  //   if(xTotal < yTotal){
+  //     cout << "from the second peramiter: ";
+  //     master = yTotal;
+  //     }
+  //   else if(yTotal < xTotal){
+  //     cout << "from the first peramiter: ";
+  //     master = xTotal;
+  //   }
+  //   return master;
+  // }
+
+  // friend void printStudent(Student s){
+  //   cout << "Student ID: " << s.getID() << endl;
+  //   cout << "Student Name: " << s.getName() << endl;
+    
+  //   s.getScore(); 
+  // }
+
+};
+
+class Student2{
+  private:
+    int sid;
+    string sname;
+    vector <double> score;
+  public:
+  Student2(){
+    sid = 0;
+    sname = "No_Name";
+    // srand(time(NULL));
+    // for(int i = 0; i < 5; i++){
+    //   score.push_back((rand() % 50));
+    //   cout << score[i] << " " << endl;
+    };
+  
+  Student2(string n, int id, vector <double> &sc){
+    sname = n;
+    sid = id;
+    for(int i = 0; i < sc.size(); i++){
+      score.push_back(sc[i]);
+    }
+  };
+  void setID(int id){
+    sid = id;
+  }
+  void setName(string name){
+    sname = name;
+  }
+  void setScores(vector <double> &sc){
+    for(int i = 0; i < sc.size(); i++){
+      score.push_back(sc[i]);
+    }
+  }
+  void getScore(){
+    cout << "Scores: ";
+    for(int i = 0; i < score.size(); i++){
+      cout << score[i] << endl;
+    }
+  }
+  string getName(){
+    return sname;
+  }
+  int getID(){
+    return sid;
+  }
+  friend class Student;
+  friend int largerTotalScore(Student1& x ,Student2& y){
+    double xTotal = 0;
+    double yTotal = 0;
+    int master;
+    for(int i = 0; i < 5; i++){
+
+      xTotal += x.score[i];
+      yTotal += y.score[i];
+    }
+    if(xTotal < yTotal){
+      cout << "from the second peramiter: ";
+      master = yTotal;
+      }
+    else if(yTotal < xTotal){
+      cout << "from the first peramiter: ";
+      master = xTotal;
+    }
+    return master;
+  }
+
+  friend void printStudent(Student1 s){
+    cout << "Student ID: " << s.getID() << endl;
+    cout << "Student Name: " << s.getName() << endl;
+    
+    s.getScore(); 
+  }
+
+};
+
+#endif
