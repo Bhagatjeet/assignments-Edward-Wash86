006e1ac2c4f1269f7fb8941fc1eeb27c2d5e8da5
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-09 13:41:43 +0000

    Updated

--- Assignment14-3/Assignment14-3.cpp
+++ Assignment14-3/Assignment14-3.cpp
@@ -1,2 +1,71 @@
 #include <iostream>
-#include "Assignment14-3.hpp"
+#include "Assignment14-3.hpp"#include <iostream>
+
+using namespace std;
+
+class Square{
+  private:
+    int width;
+  public:
+    Square(){
+      width = 100;
+    };
+    void setWidth(int w){
+      width = w;
+    };
+    int getWidth(){
+      return width;
+    };
+    int getArea(){
+      return width * 4;
+    };
+    friend class Rectangle;
+};
+
+class Rectangle{
+  private:
+    double width;
+    double height;
+  public:
+  Rectangle(){
+    width = 12;
+    height = 45;
+  };
+  void setWidth(double w){
+    width = w;
+  };
+  void setHeight(double h){
+    height = h;
+  };
+  double getHeight()const {
+    return height;
+  };
+  double getWidth() const{
+    return width;
+  };
+  double getArea()const {
+    return height * width;
+  };
+  Rectangle makeRectanglewithSquare(Square s){
+    Rectangle square;//Object
+    
+
+    square.setWidth(s.getWidth());
+    return square; //Need to return a class object;
+  }
+};
+
+int main() {
+
+  Square ana;
+  Rectangle kim, nads;
+
+  ana.setWidth(8);
+
+ 
+
+ nads = kim.makeRectanglewithSquare(ana);
+
+ cout << nads.getWidth() << endl;
+ 
+} 
