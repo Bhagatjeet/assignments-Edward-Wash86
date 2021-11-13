fed0cbe3316df0e75fe4b621edccea5cae0e537d
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-13 21:49:03 +0000

    updated

--- Assignment14-4/14-4.cpp
+++ Assignment14-4/14-4.cpp
@@ -8,9 +8,21 @@ class Numbers{
     int size;
     int *numbers; //pointer
   public:
-    Numbers(); // default constructor
-    Numbers(int s);
-    Numbers(const Numbers &); //referance
+    Numbers(){
+      size = 0;
+      numbers = nullptr;
+    }; // default constructor
+    Numbers(int s){
+      size = s;
+      numbers = new int [size];
+      for(int i = 0; i < size; i++){
+        numbers[i] = 674 * s[i];
+        }
+    };
+    Numbers(const Numbers &){
+      this -> size;
+      this -> numbers = new int[size];
+    }; //referance
     ~Numbers(); //Destructor
     int getMAx() const;
     friend void printNumbers();
