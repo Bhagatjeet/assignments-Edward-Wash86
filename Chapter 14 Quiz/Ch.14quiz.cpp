588f16d4cb6a1ae0ace679e9ab39ae76a1480d8a
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-23 23:59:22 +0000

    started

---
+++ Chapter 14 Quiz/Ch.14quiz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include
+
+using namespace std;
+
+class Course{
+  private:
+    string courseName, semester;
+    int credits;
+    vector <Student> student;  
+  public:
+    void setCourseName(string){
+
+    };
+}
+
+class student{
+  int ID;
+  string studentName;
+  char grade;
+  double scores;
+}
+
+int main() {
+  
+} 
