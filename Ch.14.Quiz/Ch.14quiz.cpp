0c1885b21142f0ad277718ea73a1f4d581e516d2
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-11-26 17:11:07 +0000

    updated

---
+++ Ch.14.Quiz/Ch.14quiz.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Students.hpp"
+#include "Course.hpp"
+
+using namespace std;
+
+int Course :: NUM_COURSES = 0;
+static int POS;
+
+
+void operator>>(fstream& IMPORT, Course& array){
+  
+  
+  IMPORT.open("CourseStudents.txt", ios:: in);
+  
+  string name, semester;
+  int credits, numberOfStudents, sid;
+  char grade;
+  double score;
+  
+  //cout << "starting at: " << POS << endl;
+  IMPORT.seekg(POS);
+  
+  
+
+  IMPORT >> name;
+  array.setCourseName(name);
+  IMPORT >> credits;
+  array.setCredits(credits);
+  IMPORT >> semester;
+  array.setSemester(semester);
+
+  IMPORT >> numberOfStudents;
+
+  for(int i = 0; i < numberOfStudents; i++){
+    IMPORT >> sid;
+    IMPORT >> name;
+    IMPORT >> grade;
+    IMPORT >> score;
+    array.setStudentFile(sid,name,score);
+  }
+
+  
+  POS = IMPORT.tellg();//Getting the position of the indicator
+  POS += 1;
+  
+    
+  
+  IMPORT.close();
+
+    
+  
+  
+
+}
+ostream& operator<<(ostream& COUT, Course& E){
+  COUT << endl;
+  COUT << "Course: " << E.getCourseName() << endl; 
+  COUT << "Credits: " << E.getCredits() << endl;
+  COUT << "Semester: " << E.getSemester() << endl;
+  COUT << "     ";
+  E.getStudent1();
+  return COUT;
+};
+
+
+
+
+int main() {
+  srand(time(NULL));
+
+  fstream courseFile, addStudent;
+
+
+  Course courses[6];
+
+  //name, semester, numOfCredits
+  //Course CS1("CS1", "Fall 2021",4);
+  //Course CS2;
+  
+  //CS1.setStudent();
+  cout << endl;
+  
+  
+  //CS1.getStudent();
+
+  for(int i = 0; i < 4; i++){
+    courseFile >> courses[i];
+  }
+  
+
+  addStudent.open("addstudent.txt", ios :: in);
+  string courseName, sName;
+  int students,sid;
+  char grade;
+  double score;
+
+  
+  for(int q = 4; q<6; q++){
+    addStudent >> courseName;
+    addStudent >> students;
+    for(int w = 0; w < 4; w++){
+            if(courses[w].getCourseName() == courseName){
+        for(int i = 0; i < students; i++){
+          addStudent >> sid;
+          addStudent >> sName;
+          addStudent >> grade;
+          addStudent >> score;
+          courses[w].setStudentFile(sid, sName, score);
+        }
+      }
+    }    
+  }
+
+  addStudent.close();
+
+for(int i = 0; i < 4; i++){
+    cout << courses[i];
+  }
+  
+  
+
+  
+} 
---
+++ Ch.14.Quiz/Students.hpp
@@ -0,0 +1,64 @@
+#ifndef 	STUDENTS_H
+#define 	STUDENTS_H
+#include <string>
+
+using namespace std;
+
+class Student{
+  private:  
+    int ID;
+    string studentName;
+    char grade;
+    double scores;
+  public:
+    Student(){
+      ID = 0;
+      studentName = "No Data";
+      
+      scores = 0;
+    };
+    Student(int id, string name, double score){
+      ID = id;
+      studentName = name;
+      scores = score;
+      if (score >= 90){
+        grade = 'A';
+      } else if (score >= 80) {
+        grade = 'B';
+      } else if (score >= 70) {
+        grade = 'C';
+      } else if (score >= 60) {
+        grade = 'D';
+      } else if (score < 60) {
+        grade = 'W';
+      } 
+    }
+    void setID(int id){
+      ID = id;
+    };
+    void setStudentName(string name){
+      studentName = name;
+    };
+    void setGrade(char GRADE){
+      grade = GRADE;
+    };
+    void setScores(double SCORE){
+      scores = SCORE;
+    };
+    int getID(){
+      return ID;
+    };
+    string getStudentName(){
+      return studentName;
+    };
+    char getGrade(){
+      return grade;
+    };
+    double getScores(){
+      return scores;
+    };
+
+    friend class Course;
+};
+
+#endif
