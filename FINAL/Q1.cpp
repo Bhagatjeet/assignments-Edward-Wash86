beaa12ecf4242f5454d2602f2d3054cadaf676bb
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-12-11 19:59:38 +0000

    Polishing user interface

--- FINAL/Q1.cpp
+++ FINAL/Q1.cpp
@@ -19,7 +19,7 @@ Implement the full source program and show the results with the various test dat
 
 #include <iostream>
 #include <fstream>
-#include "Course.hpp"
+#include "Course.cpp"
 
 Course recursiveBinarySearch(int , Course array[], int , int );
 Course iterativeBinarySearch(Course array[], int, int);
@@ -28,6 +28,7 @@ void bubbleSortbyID(Course array[], int );
 using namespace std;
 
 static int POS;
+static string fileName;
 const int COURSES = 10;
 
 void operator>>(fstream& FILE_IN, Course& array){
@@ -35,7 +36,7 @@ void operator>>(fstream& FILE_IN, Course& array){
   string name;
   int credit;
   
-  FILE_IN.open("CourseSet2.txt", ios :: in);
+  FILE_IN.open(fileName, ios :: in);
 
   FILE_IN.seekg(POS);
 
@@ -66,8 +67,31 @@ int main() {
   fstream FILE_IN;
   Course lessons[10];
 
+  int selection;
+  cout << "Please Select a file to search: " << endl;
+  cout << "1. CourseSet1.txt" << endl;
+  cout << "2. CourseSet2.txt" << endl;
+  cout << "3. CourseSet3.txt" << endl;
+  cout << endl;
+  cout << "Search file: ";
+  cin >> selection;
+
+  switch(selection){
+    case 1: fileName = "FINAL/CourseSet1.txt";
+            cout << "   Searching CourseSet1.txt" << endl;
+            break;
+    case 2: fileName = "FINAL/CourseSet2.txt";
+            cout << "   Searching CourseSet2.txt" << endl;
+            break;
+    case 3: fileName = "FINAL/CourseSet3.txt";
+            cout << "   Searching CourseSet3.txt" << endl;
+            break;
+    default: cout << "    File selection is not valid." << endl;
+  }
+
+
   int find;
-  cout << "Enter a ID number: ";
+  cout << "   Enter a ID number: ";
   cin >> find;
   cout << endl;
 
