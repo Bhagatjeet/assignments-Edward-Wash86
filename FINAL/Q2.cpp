5d5438ba1145144d2161fc7e8ae3067c33b0c56b
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-12-11 22:35:59 +0000

    I felt that bringing the menu code from Q1 would be the best way to perform the test on the different data files.

--- FINAL/Q2.cpp
+++ FINAL/Q2.cpp
@@ -1,3 +1,10 @@
+/*
+In this question, we will implement the Recursive Quick Sort program with the Class Course. We need to sort 10 Course objects based on the Course ID(sort key = ID). We will use the same class Course that was implemented in Question 1
+Show the Recursion-Call hierarchy with the typical parameter values that were used.
+example; the parameters in this example just denotes the range of the slit. 
+Implement the full source program and show the results with the various test data.
+*/
+
 #include <iostream>
 #include <fstream>
 #include "Course.hpp"
@@ -6,3 +13,47 @@ int partition (Course array[], int , int );
 void quickSort(Course array[], int, int );
 void swap(int* a, int* b);
 void displayArray(Course arr[], int size);
+
+int main() {
+
+  fstream FILE_IN;
+  Course lessons[10];
+
+  int selection;
+  cout << "Please Select a file to search: " << endl;
+  cout << "1. CourseSet1.txt" << endl;
+  cout << "2. CourseSet2.txt" << endl;
+  cout << "3. CourseSet3.txt" << endl;
+  cout << endl;
+  cout << "Search file: ";
+  cin >> selection;
+
+  cout << endl;
+
+  switch(selection){
+    case 1: fileName = "CourseSet1.txt";
+            cout << "Sorting CourseSet1.txt" << endl;
+            break;
+    case 2: fileName = "CourseSet2.txt";
+            cout << "Sorting CourseSet2.txt" << endl;
+            break;
+    case 3: fileName = "CourseSet3.txt";
+            cout << "Sorting CourseSet3.txt" << endl;
+            break;
+    default: cout << "File selection is not valid." << endl;
+  }
+  
+  cout << endl;
+
+  for(int i = 0; i < COURSES; i++){
+    FILE_IN >> lessons[i];
+    
+  }
+
+  int n = sizeof(lessons)/sizeof(lessons[0]);
+
+  quickSort(lessons, 0, n-1);
+  
+  displayArray(lessons,n);
+  
+} 
