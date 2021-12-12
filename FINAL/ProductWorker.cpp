9468f9e058923f89b16ece22d78a503a4b8e1ff5
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-12-12 19:47:11 +0000

    updated derived cpp constructor

--- FINAL/ProductWorker.cpp
+++ FINAL/ProductWorker.cpp
@@ -1,48 +1,14 @@
-/*
-#ifndef PRODUCTWORKER_H
-#define PRODUCTWORKER_H
-
-using namespace std;
-
-class ProductWorker : public Employee{
-  - int shift;
-  - double hourlyPayRate;
-  ProductionWorker(string name, int id, int hired, int shift, double pay );
-  + void setShift(int shift);
-  + void setPay(double money);
-  + int getShift();
-}
-
-#endif
-=====================================================
-
-class Employee{
-  private:
-    string employeeName;
-    int employeeNumber;
-    string hireDate;
-  public:
-    Employee();
-    Employee(string name, int id, string hired);
-    void setName(string name);
-    void setID(int id);
-    void setHired(string hired);
-    string getName();
-    int getID();
-    string getHired(); 
-};
-*/
-
 #include <iostream>
 #include "productworker.hpp"
 #include "employee.hpp"
 
+
 using namespace std;
 
-ProductWorker :: ProductWorker(int SHIFT, double pay) : Employee(name,id,hired){
+ProductWorker :: ProductWorker(string name, int id, string hired, int SHIFT, double pay) : Employee(name,id,hired){
+  employeeName = name;
+  employeeNumber = id;
+  hireDate = hired;
   shift = SHIFT;
   hourlyPayRate = pay;
-} 
-
-
-
+};
