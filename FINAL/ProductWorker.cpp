edb391568fb7c114168c8470951566c4b7ab0ec1
Edward Washington <68512860+Edward-Wash86@users.noreply.github.com>
2021-12-12 19:21:35 +0000

    update, having trouble with the inheritance

--- FINAL/ProductWorker.cpp
+++ FINAL/ProductWorker.cpp
@@ -1,16 +1,48 @@
+/*
 #ifndef PRODUCTWORKER_H
 #define PRODUCTWORKER_H
-#include "employee.hpp"
 
 using namespace std;
 
 class ProductWorker : public Employee{
   - int shift;
   - double hourlyPayRate;
-  ProductionWorker(int shift, double pay) : Employee(name, id, hired);
+  ProductionWorker(string name, int id, int hired, int shift, double pay );
   + void setShift(int shift);
   + void setPay(double money);
   + int getShift();
 }
 
 #endif
+=====================================================
+
+class Employee{
+  private:
+    string employeeName;
+    int employeeNumber;
+    string hireDate;
+  public:
+    Employee();
+    Employee(string name, int id, string hired);
+    void setName(string name);
+    void setID(int id);
+    void setHired(string hired);
+    string getName();
+    int getID();
+    string getHired(); 
+};
+*/
+
+#include <iostream>
+#include "productworker.hpp"
+#include "employee.hpp"
+
+using namespace std;
+
+ProductWorker :: ProductWorker(int SHIFT, double pay) : Employee(name,id,hired){
+  shift = SHIFT;
+  hourlyPayRate = pay;
+} 
+
+
+
